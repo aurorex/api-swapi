e74d13da1acfeefbe378ea7347125d00fa7d4f65
aurorex <selva_beat@hotmail.com>
2018-02-14 20:37:05 +0100

    recorriendo imagenes y agregando funcionalidad onclick para consumir recurso de la api

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,21 @@
-/* VERSIÓN 1 */
-$(document).ready(function() {
+const imgApi = document.querySelectorAll('.image-api');
 
-});
+console.log(imgApi);
 
+for (let i = 0;i < imgApi.length;i++) {
+  console.log(imgApi[i]);
 
-/* VERSIÓN 2 */
-function begin() {
-
+  imgApi[i].onclick = function() {
+    let targetImage = this.getAttribute('data-target'); 
+    
+    fetch(`https://swapi.co/api/people/${targetImage}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((recurso)=>{
+        console.log(recurso);
+      // addNews(JSON.parse(recurso));
+      });
+  };
 }
-$(document).ready(begin);
+
